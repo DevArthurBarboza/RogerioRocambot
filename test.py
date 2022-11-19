655edb3edb947f22ca01382174c37c835821764a
DevArthurBarboza <arthur.barboza0016@gmail.com>
2022-11-19 20:54:25 -0300

    :art: Improving bot communication - turn on/off

--- test.py
+++ test.py
@@ -5,30 +5,55 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 from nltk_teste import Nltk as nl
+from bot import Bot
+from env import Env
 
 
 nl_instance = nl()
 nl_instance.calc()
 
+TOKEN = Env.get_env('DISCORD_TOKEN')
+COMMAND_PREFIX = Env.get_env('COMMAND_PREFIX')
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents = intents)
+bot = commands.Bot(command_prefix=COMMAND_PREFIX,intents=intents)
 
-@client.event
+@bot.command('start')
+async def start(ctx):
+    Bot.start()
+    await ctx.send("Hi Sir ! Rogério Rocambot reporting to duty !")
+
+@bot.command('stop')
+async def stop(ctx):
+    Bot.stop()
+    await ctx.send("See you soon !")
+
+
+@bot.event
+async def on_ready():
+    return 
+
+@bot.event
 async def on_member_join(member):
     response = 'mais um bobão entrou no chat'
     await message.channel.send(response)
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    await bot.process_commands(message)
+    
+    if (Bot.is_command(bot.commands,message.content)):
+        return 
+    
+    if not(Bot.is_turned_on()):
+        return
+
+    if message.author == bot.user:
         return
 
     response = nl_instance.receive(message.content)
     await message.channel.send(response)
     
-client.run(TOKEN)
+bot.run(TOKEN)
