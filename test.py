6f6698a27b375f825b1887473f890f58beb89352
DevArthurBarboza <arthur.barboza0016@gmail.com>
2022-11-18 22:03:40 -0300

    :tada: Conexão com Discord estabelecida

---
+++ test.py
@@ -0,0 +1,14 @@
+import os
+import random
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents = intents)
+
+client.run(TOKEN)
